429f5d171915c9c8ecaa3f9621e824728119bc41
JConan <conanjahh@hotmail.com>
2024-08-06 04:20:57 +0200

    fix dev env initialisation

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const isDev = process.env.MODE === 'development';
+const isDev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
