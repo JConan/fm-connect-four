8e2c89e1c8e3e4b19c9e555ce79183b46ecf3a6c
JConan <conanjahh@hotmail.com>
2024-08-10 09:29:58 +0200

    add in game menu dialog

--- src/lib/components/GameMenu.svelte
+++ src/lib/components/GameMenu.svelte
@@ -3,20 +3,35 @@
 	import { route } from '$lib/ROUTES';
 	import { resetBoard } from '$lib/stores/board';
 	import Image from '$lib/components/Image.svelte';
+	import { goto } from '$app/navigation';
+
+	let dialog: HTMLDialogElement;
+
+	const onMenu = () => dialog.showModal();
+	const onContinue = () => dialog.close();
+	const onRestart = () => {
+		resetBoard();
+		dialog.close();
+	};
+	const onQuit = () => {
+		dialog.close();
+		goto(`${base}${route('/')}`);
+	};
 </script>
 
 <div class="menu">
-	<a href={`${base}${route('/')}`} class="flat-button button-purple">Menu</a>
+	<button class="flat-button button-purple" on:click={onMenu}>Menu</button>
 	<Image class="logo" name="logo" />
-	<button
-		class="flat-button button-purple"
-		on:click={() => {
-			resetBoard();
-		}}
-	>
-		Restart
-	</button>
+	<button class="flat-button button-purple" on:click={onRestart}> Restart </button>
 </div>
+
+<dialog bind:this={dialog}>
+	<span>PAUSE</span>
+	<button class="text-button raised-button button-white" on:click={onContinue}>Continue game</button
+	>
+	<button class="text-button raised-button button-white" on:click={onRestart}>Restart</button>
+	<button class="text-button raised-button button-red" on:click={onQuit}>Quit Game</button>
+</dialog>
 
 <style>
 	.menu {
@@ -29,5 +44,31 @@
 	:global(.logo) {
 		width: 40px;
 		height: 40px;
+	}
+
+	dialog[open] {
+		padding: 40px 0;
+		width: 480px;
+		height: 491px;
+		border: 3px solid var(--black);
+		background: var(--purple);
+		box-shadow: 0px 10px 0px 0px var(--black);
+		border-radius: 40px;
+		font-size: 56px;
+		color: white;
+		font-weight: 700;
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-template-rows: 1fr repeat(3, 0.8fr);
+		place-items: center;
+
+		& .text-button {
+			grid-template-columns: 1fr;
+			text-align: center;
+		}
+
+		&::backdrop {
+			background: #00000080;
+		}
 	}
 </style>
